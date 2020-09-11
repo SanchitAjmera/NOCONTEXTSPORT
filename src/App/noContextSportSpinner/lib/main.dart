a025fa1abf19284ac90e912489b5eeb81a3cbc1e
sa3119 <sanchitajmera2017@gmail.com>
2020-09-11 04:28:41 +0100

    created main page and sub page

--- src/App/noContextSportSpinner/lib/main.dart
+++ src/App/noContextSportSpinner/lib/main.dart
@@ -14,7 +14,59 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'NOCONTEXTAPP-spinner '),
+      home: MainPage(),
+    //  home: MyHomePage(title: 'NOCONTEXTAPP-spinner '),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NOCONTEXTSPORT'),
+      ),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Click button to start game'),
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child:Text('Start'),
+              onPressed: () {
+                navigateToSubPage(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SubPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('This is a subpage'),
+          /*  RaisedButton(
+              textColor: Colors.white,
+              color: Colors.redAccent,
+              child:Text('Back to Main Page'),
+              onPressed: () {
+          //      Navigator.pop(context);
+              }
+            )*/
+          ],
+        ),
+      ),
     );
   }
 }
@@ -22,30 +74,31 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-
       body: Center(
       ),
     );
   }
+}
+
+
+Future navigateToSubPage(context) async {
+  Navigator.push(context, MaterialPageRoute(builder: (context) =>
+  SubPage()));
 }
