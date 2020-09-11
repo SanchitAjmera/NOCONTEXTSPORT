08f5f63990fb7d0c45fdca9ced0f672836f54073
sa3119 <sanchitajmera2017@gmail.com>
2020-09-11 04:28:58 +0100

    created spinner class

---
+++ src/App/noContextSportSpinner/lib/spinner.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart.math';
+
+class Spinner extends StatefulWidget {
+  @override
+  _SpinnerState createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<Spinner> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  Animation<double> animation_rotation;
+  Animation<double> animation_radius_in;
+  Animation<double> animation_radius_out;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      child: Center{
+        child: Stack(
+          children: <Widget>[
+            Dot(
+              radius: 30.0,
+              color: Colors.black12,
+            ),
+            Dot(
+              radius: 5.0,
+              color: Colors.redAccent,
+            ),
+
+            Transform.translate(
+              offset: Offset(cos(pi), sin(pi)),
+              child : Dot(
+                radius: 5.0,
+                color: Colors.redAccent,
+              ),
+            )
+          ],
+        ),
+      },
+    );
+  }
+}
+
+class Dot extends StatelessWidget {
+
+  final double radius;
+  final Color color;
+
+  Dot({this.radius, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.radius,
+      height: this.radius,
+      decoration: BoxDecoration(
+        color: this.color,
+        shape: BoxShape.circle
+      )
+    );
+  }
+}
