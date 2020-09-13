cf26aac18f8aa1c96fa1fd82a92743561203e3d9
sa3119 <sanchitajmera2017@gmail.com>
2020-09-13 22:07:25 +0100

    displaying name as result rather than picture

--- src/App/noContextSportSpinner/lib/home_page.dart
+++ src/App/noContextSportSpinner/lib/home_page.dart
@@ -19,20 +19,19 @@ class _HomePageState extends State<HomePage>
   AnimationController _ctrl;
   Animation _ani;
   List<Luck> _items = [
-    Luck("apple", Colors.accents[0]),
-    Luck("raspberry", Colors.accents[2]),
-    Luck("grapes", Colors.accents[4]),
-    Luck("fruit", Colors.accents[6]),
-    Luck("milk", Colors.accents[8]),
-    Luck("salad", Colors.accents[10]),
-    Luck("cheese", Colors.accents[12]),
-    Luck("carrot", Colors.accents[14]),
+    Luck("Sami", Colors.accents[0]),
+    Luck("Rajun", Colors.accents[2]),
+    Luck("Max", Colors.accents[4]),
+    Luck("Sanch", Colors.accents[6]),
+    Luck("Emir", Colors.accents[8]),
+    Luck("Ali", Colors.accents[10]),
   ];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // time duration of wheel spinning
     var _duration = Duration(milliseconds: 5000);
     _ctrl = AnimationController(vsync: this, duration: _duration);
     _ani = CurvedAnimation(parent: _ctrl, curve: Curves.fastLinearToSlowEaseIn);
@@ -50,7 +49,9 @@ class _HomePageState extends State<HomePage>
         child: AnimatedBuilder(
             animation: _ani,
             builder: (context, child) {
+              // value is the future endpoint of the wheel
               final _value = _ani.value;
+              // angle is angle the wheel spins
               final _angle = _value * this._angle;
               return Stack(
                 alignment: Alignment.center,
@@ -98,18 +99,24 @@ class _HomePageState extends State<HomePage>
   }
 
   int _calIndex(value) {
+    // base is the index of the randomly chosen component
     var _base = (2 * pi / _items.length / 2) / (2 * pi);
     return (((_base + value) % 1) * _items.length).floor();
   }
-
+  // function which outputs results at the bottom of the screen
   _buildResult(_value) {
     var _index = _calIndex(_value * _angle + _current);
+    // items[index] refers to the chosen picture to display
     String _asset = _items[_index].asset;
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 16.0),
       child: Align(
         alignment: Alignment.bottomCenter,
-        child: Image.asset(_asset, height: 80, width: 80),
+        child: new Text(
+          _asset,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+        )//Image.asset(_asset, height: 80, width: 80),
       ),
     );
   }
