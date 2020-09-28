882c53d6c434b5b2f813b6c0d6d055c05f171090
sa3119 <sanchitajmera2017@gmail.com>
2020-09-28 20:50:42 +0100

    Added onTap return to spinner functionality

--- src/App/noContextSportSpinner/lib/truth_page.dart
+++ src/App/noContextSportSpinner/lib/truth_page.dart
@@ -15,13 +15,14 @@ import 'truths.dart';
 class TruthPage extends StatefulWidget {
 
   String name;
+  List<String> names;
 
   //contructor enabling passing of name details
-  TruthPage({Key key, @required this.name}) : super(key: key);
+  TruthPage({Key key, @required this.name, @required this.names}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return _TruthPageState(name);
+    return _TruthPageState(name, names);
   }
 }
 
@@ -29,11 +30,13 @@ class _TruthPageState extends State<TruthPage>
     with SingleTickerProviderStateMixin {
 
   String name;
+  List<String> names;
   Color _colorContainer1 = Colors.white;
   Truths truths = new Truths();
 
-  _TruthPageState(String name){
+  _TruthPageState(String name, List<String> names){
     this.name  = name;
+    this.names = names;
   }
 
   @override
@@ -45,31 +48,39 @@ class _TruthPageState extends State<TruthPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => navigateToSubPage(context),
+        child: new Container(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.pinkAccent[400].withOpacity(0.8), Colors.deepOrangeAccent[400].withOpacity(0.7)])),
-        child: Center(
-          child: new AlertDialog(
-            contentPadding: EdgeInsets.all(0.0),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                InkWell(
-                  child: Container(
-                    width: 300,
-                    height: 70,
-                    color: _colorContainer1,
-                    child: new Text(truths.getTruth()),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.pinkAccent[400].withOpacity(0.8), Colors.deepOrangeAccent[400].withOpacity(0.7)])),
+          child: Center(
+            child: new AlertDialog(
+              contentPadding: EdgeInsets.all(0.0),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  InkWell(
+                    child: Container(
+                      width: 300,
+                      height: 70,
+                      color: _colorContainer1,
+                      child: new Text(truths.getTruth()),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(names: names)));
   }
 }
