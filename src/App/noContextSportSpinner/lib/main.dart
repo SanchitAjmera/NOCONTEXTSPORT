f6351b10508ef5bd8a6b589c899e76dfb11dc131
sa3119 <sanchitajmera2017@gmail.com>
2020-10-08 05:22:51 +0100

    restructured mainpage

--- src/App/noContextSportSpinner/lib/main.dart
+++ src/App/noContextSportSpinner/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Schyler'),
       home: MainPage(),
     );
   }
@@ -23,14 +23,17 @@ class MainPage extends StatefulWidget {
 class _mainPageState extends State<MainPage> {
   // collection of names inputed by player
   List<String> names = [];
-  int _count = 0;
+  List<Widget> _listings = [];
+  int count = 0;
   TextEditingController eCtrl = new TextEditingController();
   bool showDialog = false;
 
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = new List.generate(count, (int i) => new InputWidget(i));
     return Scaffold(
+    //  resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -38,90 +41,110 @@ class _mainPageState extends State<MainPage> {
                 end: Alignment.bottomCenter,
                 colors: [Colors.pinkAccent[400].withOpacity(0.8), Colors.deepOrangeAccent[400].withOpacity(0.7)])),
 
-        child: new Column(
+        child: new Stack(
+          alignment: Alignment.topCenter,
           children: <Widget>[
-
-            new Flexible(
-              child: new ListView.builder(
-                itemCount: names.length,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  return new Column(
-                    children: <Widget>[
-                      new Text(
-                        names[index],
-                        style: TextStyle(fontSize: 25),
-                        textAlign: TextAlign.center)
-                    ],
-                  );
-                }
+              Column (
+              // child: new ListView.builder(
+              //   itemCount: names.length,
+              //   itemBuilder: (BuildContext ctxt, int index) {
+              //     return new Column(
+              //       children: <Widget>[
+              //         new Text(
+              //           names[index],
+              //           style: TextStyle(fontSize: 25),
+              //           textAlign: TextAlign.center)
+              //       ],
+              //     );
+              //   }
+              // ),
+                children: children,
+              ),
+            Positioned(
+              width: 300.0,
+              height: 100.0,
+              top: 100,
+              child : Container(
+                child: Center(
+                  child: new Image.asset('asset/image/sportify.png'),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(bottom: 0),
-              child: Center(
-                child: new AlertDialog(
-                  contentPadding: EdgeInsets.only(
-                    left: 40,
-                    right: 20,
-                  ),
-                  //  title: new Text("Enter Name"),
-                  content: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(0),
-                        width: 130.0,
-                        child: Center(
-                          child: TextField(
-                            style: TextStyle(fontSize: 20),
-                        //    textAlign: TextAlign.center,
-                            controller: eCtrl,
-                            decoration: new InputDecoration.collapsed( hintText: "ENTER NAME"),
-                            maxLines: 1,
-                            onSubmitted: (String text){}
+            Positioned(
+              width: 400.0,
+              height: 120.0,
+              top: 300,
+              child: Container(
+                child: Center(
+                  child: new AlertDialog(
+                    contentPadding: EdgeInsets.only(
+                      left: 40,
+                      right: 0,
+                    ),
+                    //  title: new Text("Enter Name"),
+                    content: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(0),
+                          width: 180.0,
+                          child: Center(
+                            child: TextField(
+                              style: TextStyle(fontSize: 30),
+                          //    textAlign: TextAlign.center,
+                              controller: eCtrl,
+                              decoration: new InputDecoration.collapsed( hintText: "Enter Name"),
+                              maxLines: 1,
+                              onSubmitted: (String text){}
+                            ),
                           ),
                         ),
-                      ),
-                      new Spacer(),
-  //                    new Spacer(),
-                      Container(
-                        width: 40.0,
-                        child: new FlatButton (
-                          onPressed: () {
-                            setState(() {
-                              if (eCtrl.text != ""){
-                                names.add(eCtrl.text);
-                              }
-                              eCtrl.clear();
-                            });
-                          },
-                          child: new Icon(Icons.add_circle_outline),
+                        new Spacer(),
+    //                    new Spacer(),
+                        Container(
+                          width: 80.0,
+                          child: new FlatButton (
+                            onPressed: () {
+                              setState(() {
+                                if (eCtrl.text != ""){
+                                  names.add(eCtrl.text);
+                                //  _listings.add(Text("hello"));
+                                }
+                                eCtrl.clear();
+                              });
+                            },
+                            child: new Icon(Icons.add_circle_outline, size: 40.0),
+                          ),
                         ),
-                      ),
-                      Container(
-                        width: 40.0,
-                        child: new FlatButton (
-                          onPressed: () {
-                            setState(() {
-                              if (eCtrl.text != ""){
-                                names.add(eCtrl.text);
-                              }
-                              eCtrl.clear();
-                              navigateToSubPage(context);
-                            });
-                          },
-                          child: new Icon(Icons.arrow_forward),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-
-
-
+            Positioned(
+              top: 410,
+              width: 300.0,
+              height: 70.0,
+              child: Container(
+                child: FlatButton(
+                  color: Colors.grey[900],
+                  textColor: Colors. white,
+                  onPressed: () {
+                    setState(() {
+                      if (eCtrl.text != ""){
+                        names.add(eCtrl.text);
+                        count = count +1;
+                      }
+                      eCtrl.clear();
+                      navigateToSubPage(context);
+                    });
+                  },
+                  child: const Text('Play', style: TextStyle(fontSize: 40)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -133,5 +156,17 @@ class _mainPageState extends State<MainPage> {
     List<String> n2 = names;
     names = [];
     Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(names: n2)));
+  }
+}
+
+class InputWidget extends StatelessWidget {
+
+  final int index;
+
+  InputWidget(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text("InputWidget: " + index.toString());
   }
 }
