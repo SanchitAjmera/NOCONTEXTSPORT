ba9cffbe36985784d03c6ce3a64edaced4a2548b
sa3119 <sanchitajmera2017@gmail.com>
2020-11-21 06:53:55 +0000

    Modified options display

--- src/App/noContextSportSpinner/lib/mode_page.dart
+++ src/App/noContextSportSpinner/lib/mode_page.dart
@@ -1,15 +1,6 @@
-import 'dart:math';
-import 'dart:collection';
 import 'package:flutter/material.dart';
-import 'main.dart';
-import 'board_view.dart';
-import 'model.dart';
-import 'questions.dart';
 import 'home_page.dart';
 import 'dart:async';
-import 'dart:io';
-import 'question_page.dart';
-import 'TD.dart';
 
 class ModePage extends StatefulWidget {
   List<String> names;
@@ -25,14 +16,31 @@ class ModePage extends StatefulWidget {
 
 class _ModePageState extends State<ModePage> {
   List<String> names;
-  List<double> positions = [50, 195, 340, 490];
   List<Color> optionColors = <Color>[
     Colors.white,
     Colors.white,
     Colors.white,
     Colors.white
   ];
+  List<String> options = [
+    "FAM FUN",
+    "ON THE SESH",
+    "ICE BREAKER",
+    "PILLOW TALK"
+  ];
+  List<String> emojis = ["apple.png", "carrot.png", "cheese.png", "grapes.png"];
+  List<String> desc = [
+    "Normal game family friendly dw",
+    "Perfect for when out wiv the lads",
+    "On a date? Lets break the ice",
+    "Spice it up in the bedroom ;)"
+  ];
   int option;
+  double width;
+  double height;
+  double fontSize;
+  double boxSize;
+  double sizeRatio = 0.70;
 
   _ModePageState({Key key, @required this.names});
 
@@ -43,6 +51,9 @@ class _ModePageState extends State<ModePage> {
 
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width * sizeRatio;
+    height = MediaQuery.of(context).size.height * 0.9;
+    boxSize = height / 4.5;
     return Scaffold(
         body: Container(
       decoration: BoxDecoration(
@@ -63,36 +74,45 @@ class _ModePageState extends State<ModePage> {
   }
 
   Widget setOption(int option) {
+    double topMargin = height / 8;
+    double top = option == 0 ? (topMargin) : option * (boxSize) + topMargin;
+    double heading_size = (width / 1.3) / options[option].length;
     return Positioned(
-      width: 350.0,
-      height: 170.0,
-      top: positions[option],
+      width: width,
+      height: boxSize,
+      top: top,
       child: Container(
-        margin: const EdgeInsets.all(30),
+        margin: const EdgeInsets.only(top: 10),
+        padding: EdgeInsets.all(0),
         child: RaisedButton(
           shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.white, width: 10)),
+            borderRadius: BorderRadius.circular(5),
+          ),
           color: optionColors[option],
           child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Container(
-                  padding: EdgeInsets.all(0),
+                  padding: EdgeInsets.all(width / 30),
                   //color: Colors.black,
-                  width: 100.0,
+                  width: width / 3,
                   // height: 190.0,
-                  child: (Text(
-                    "Image 1",
-                  )),
+                  child: new Image.asset('asset/image/' + emojis[option]),
                 ),
                 Container(
-                  padding: EdgeInsets.all(0),
-                  width: 100.0,
-                  child: (Text(
-                    "Option 1, details, price",
-                  )),
+                  child: ListView(children: <Widget>[
+                    Text(
+                      options[option],
+                      style: TextStyle(fontSize: heading_size),
+                    ),
+                    Text(
+                      desc[option],
+                      style: TextStyle(color: Colors.grey[600]),
+                    )
+                  ]),
+                  padding: EdgeInsets.only(left: (width / 30)),
+                  width: ((2 * width) / 3) - 4 * (width / 30),
                 ),
               ]),
           onPressed: () {
