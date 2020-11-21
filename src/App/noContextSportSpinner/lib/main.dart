85a6e04073e517fa3c5f2556eacd105cad565b12
sa3119 <sanchitajmera2017@gmail.com>
2020-11-21 06:03:52 +0000

    Generalised sizes in splash, warning and main page

--- src/App/noContextSportSpinner/lib/main.dart
+++ src/App/noContextSportSpinner/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'NOCONTEXTSPORT',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Schyler'),
       home: SplashPage(),
     );
@@ -31,6 +31,9 @@ class StartPage extends StatefulWidget {
 }
 
 class _splashPageState extends State<SplashPage> {
+  double width;
+  double height;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -38,17 +41,15 @@ class _splashPageState extends State<SplashPage> {
     Timer(Duration(seconds: 2), () => navigateToWarningPage(context));
   }
 
-  Future navigateToWarningPage(context) async {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => WarningPage()));
-  }
-
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width * 0.80;
+    height = MediaQuery.of(context).size.height * 0.80;
     return Scaffold(
         //  resizeToAvoidBottomInset: false,
         body: GestureDetector(
       behavior: HitTestBehavior.opaque,
+      onTap: () => navigateToWarningPage(context),
       child: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -60,9 +61,8 @@ class _splashPageState extends State<SplashPage> {
             ])),
         child: new Stack(alignment: Alignment.center, children: <Widget>[
           Positioned(
-            width: 350.0,
-            height: 100.0,
-            //  top: 150,
+            width: width, //350.0,
+            top: (height / 2),
             child: Container(
               child: Center(
                 child: new Image.asset('asset/image/logo1.jpg'),
@@ -73,6 +73,11 @@ class _splashPageState extends State<SplashPage> {
       ),
     ));
   }
+
+  Future navigateToWarningPage(context) async {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => WarningPage()));
+  }
 }
 
 class _mainPageState extends State<StartPage> {
@@ -82,6 +87,13 @@ class _mainPageState extends State<StartPage> {
   int count = 0;
   TextEditingController eCtrl = new TextEditingController();
   bool showDialog = false;
+  double width;
+  double height;
+  double font_size;
+  double font_play;
+  double sizeRatio = 0.7;
+  double boxHeight;
+  double nameHeight;
 
   @override
   void initState() {
@@ -91,9 +103,13 @@ class _mainPageState extends State<StartPage> {
 
   @override
   Widget build(BuildContext context) {
-    //Future.delayed(Duration.zero, () => drinkingAgreement(context));
+    width = MediaQuery.of(context).size.width * sizeRatio;
+    height = MediaQuery.of(context).size.height * sizeRatio;
+    font_size = width / 10;
+    font_play = width / 6;
+    boxHeight = height / 7;
+    nameHeight = boxHeight / 1.8;
     return Scaffold(
-      //  resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -107,9 +123,9 @@ class _mainPageState extends State<StartPage> {
           alignment: Alignment.topCenter,
           children: <Widget>[
             Positioned(
-              width: 300.0,
-              height: 235.0,
-              top: 50,
+              width: width, //300.0,
+              height: 4 * nameHeight + 40,
+              top: height / 5,
               child: Container(
                 child: Row(
                   children: [
@@ -124,84 +140,89 @@ class _mainPageState extends State<StartPage> {
                 ),
               ),
             ),
+            // ENTER NAME BOX
             Positioned(
-              width: 400.0,
-              height: 120.0,
-              top: 300,
+              width: width,
+              height: boxHeight,
+              top: (height / 2) + boxHeight,
               child: Container(
+                decoration: new BoxDecoration(
+                  borderRadius:
+                      new BorderRadius.all(const Radius.circular(10.0)),
+                  color: Colors.white,
+                ),
+                padding: EdgeInsets.only(left: width / 14),
                 child: Center(
-                  child: new AlertDialog(
-                    contentPadding: EdgeInsets.only(
-                      left: 40,
-                      right: 0,
-                    ),
-                    //  title: new Text("ENTER NAME"),
-                    content: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(0),
-                          width: 180.0,
-                          child: Center(
-                            child: TextField(
-                                style: TextStyle(fontSize: 30),
-                                //    textAlign: TextAlign.center,
-                                controller: eCtrl,
-                                decoration: new InputDecoration.collapsed(
-                                    hintText: "ENTER NAME"),
-                                maxLines: 1,
-                                onSubmitted: (String text) {}),
-                          ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: ((2 * width) / 3),
+                        child: Center(
+                          child: TextField(
+                              style: TextStyle(fontSize: font_size),
+                              // textAlign: TextAlign.center,
+                              controller: eCtrl,
+                              decoration: new InputDecoration.collapsed(
+                                  hintText: "ENTER NAME"),
+                              maxLines: 1,
+                              onSubmitted: (String text) {}),
                         ),
-                        new Spacer(),
-                        //                    new Spacer(),
-                        Container(
-                          width: 80.0,
-                          child: new FlatButton(
-                            onPressed: () {
-                              setState(() {
-                                if (eCtrl.text != "") {
-                                  names.add(eCtrl.text);
-                                  //  _listings.add(Text("hello"));
-                                }
-                                eCtrl.clear();
-                              });
-                            },
-                            child:
-                                new Icon(Icons.add_circle_outline, size: 40.0),
-                          ),
+                      ),
+                      Container(
+                        width: (width / 3) - (width / 14),
+                        child: new FlatButton(
+                          onPressed: () {
+                            setState(() {
+                              if (eCtrl.text != "") {
+                                names.add(eCtrl.text);
+                              }
+                              eCtrl.clear();
+                            });
+                          },
+                          child: new Icon(Icons.add_circle_outline,
+                              size: width / 7),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
+                    //),
+                  ),
+                ),
+              ),
+            ),
+            // PLAY BOX
+            Positioned(
+              top: (height / 1.9) + 2 * boxHeight,
+              width: width,
+              height: boxHeight,
+              child: Container(
+                decoration: new BoxDecoration(
+                  borderRadius:
+                      new BorderRadius.all(const Radius.circular(10.0)),
+                  color: Colors.orange,
+                ),
+                child: Center(
+                  child: FlatButton(
+                    color: Colors.orange,
+                    textColor: Colors.black,
+                    onPressed: () {
+                      setState(() {
+                        if (eCtrl.text != "") {
+                          names.add(eCtrl.text);
+                          count = count + 1;
+                        }
+                        eCtrl.clear();
+                        navigateToSubPage(context);
+                      });
+                    },
+                    child: const Text('PLAY', style: TextStyle(fontSize: 40)),
                   ),
                 ),
               ),
             ),
             Positioned(
-              top: 410,
-              width: 300.0,
-              height: 70.0,
-              child: Container(
-                child: FlatButton(
-                  color: Colors.orange[600],
-                  textColor: Colors.black,
-                  onPressed: () {
-                    setState(() {
-                      if (eCtrl.text != "") {
-                        names.add(eCtrl.text);
-                        count = count + 1;
-                      }
-                      eCtrl.clear();
-                      navigateToSubPage(context);
-                    });
-                  },
-                  child: const Text('PLAY', style: TextStyle(fontSize: 40)),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 10,
+              top: (height / sizeRatio) - (height / 10),
               child: Text(
                 "BY NOCONTEXTSPORT",
                 style: TextStyle(
@@ -215,6 +236,7 @@ class _mainPageState extends State<StartPage> {
   }
 
   Widget showName(String name) {
+    double offset = name.length * 5.1;
     return new Container(
       decoration: new BoxDecoration(
           color: Colors.grey,
@@ -224,12 +246,12 @@ class _mainPageState extends State<StartPage> {
             bottomLeft: const Radius.circular(40.0),
             bottomRight: const Radius.circular(40.0),
           )),
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.only(right: 10, left: 10),
       margin: const EdgeInsets.all(5),
-      height: 50,
+      height: nameHeight,
       child: Text(
         name,
-        style: TextStyle(fontSize: 30),
+        style: TextStyle(fontSize: boxHeight / 2.5),
       ),
     );
   }
@@ -248,6 +270,11 @@ class WarningPage extends StatefulWidget {
 }
 
 class _warningPageState extends State<WarningPage> {
+  double width;
+  double fontSize;
+  double height;
+  double sizeRatio = 0.8;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -256,6 +283,9 @@ class _warningPageState extends State<WarningPage> {
 
   @override
   Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width * sizeRatio;
+    height = MediaQuery.of(context).size.height * sizeRatio;
+    fontSize = width / 8;
     return Scaffold(
         //  resizeToAvoidBottomInset: false,
         body: GestureDetector(
@@ -273,30 +303,30 @@ class _warningPageState extends State<WarningPage> {
                 child:
                     new Stack(alignment: Alignment.center, children: <Widget>[
                   Positioned(
-                    // width: 350.0,
+                    width: width,
                     // height: 100.0,
                     //  top: 150,
                     child: Container(
-                      padding: EdgeInsets.all(60),
+                      padding: EdgeInsets.all(0),
                       child: Center(
                         child: new Text(
                           "Your drinking is your own responsibility",
-                          style: TextStyle(color: Colors.white, fontSize: 50),
+                          style: TextStyle(
+                              color: Colors.white, fontSize: fontSize),
                           textAlign: TextAlign.center,
                         ),
                       ),
                     ),
                   ),
                   Positioned(
-                    // width: 350.0,
-                    // height: 100.0,
-                    bottom: 50,
+                    bottom: height / 8,
                     child: Container(
-                      padding: EdgeInsets.all(50),
+                      //padding: EdgeInsets.all(50),
                       child: Center(
                         child: new Text(
                           "Tap To Agree",
-                          style: TextStyle(color: Colors.white, fontSize: 30),
+                          style: TextStyle(
+                              color: Colors.white, fontSize: width / 12),
                           textAlign: TextAlign.center,
                         ),
                       ),
